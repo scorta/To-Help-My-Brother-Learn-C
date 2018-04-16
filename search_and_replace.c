2d8545c238a15e489ddf04c1078063d269b9ba87
Nguyen Dang Duc <scorta@users.noreply.github.com>
2018-04-17 05:13:21 +0700

    Update search_and_replace.c

--- search_and_replace.c
+++ search_and_replace.c
@@ -31,29 +31,26 @@ void Copy(char dest[], const char source[], const int from_dest, const int from_
 	for (int i = from_dest, j = from_source; i <= (to_source - from_source) + from_dest; ++i, ++j) {
 		dest[i] = source[j];
 	}
-	printf("\nCOPY dest %s \n source %s\n", dest, source);
 }
 
 char *Replace(char s[], const char s1[], const char s2[]) {
-	printf("Replace\n");
 	size_t result_length = 0;
 	size_t count = 0;
-	for (size_t i = 0; i < strlen(s) - strlen(s1) + 1; ++i) {
+	for (size_t i = 0; i < strlen(s) - strlen(s1) + 1;) {
 		if (CheckFrom(s, s1, i) >= 0) {
 			++count;
+			i += strlen(s1);
+		} else {
+			++i;
 		}
 	}
 
-	char *result = (char*)malloc(sizeof(char) * (strlen(s) + count * (strlen(s2) - strlen(s1))));
-	Copy(result, s, 0, 0, strlen(s) - 1);
-	printf("\nR - S %s - %s\n", result, s);
-
 	result_length = strlen(s) + count * (strlen(s2) - strlen(s1));
-	int temp = strlen(result);
+	char *result = (char*)malloc(sizeof(char) * result_length);
+	
 	result[result_length] = '\0';
 
-	for (int i_s = 0, pos = Search(s, s1, i_s), i = 0; pos >= 0; pos = Search(s, s1, i_s)) {
-		printf("\nResult %s %d %d %d\n", result, i, i_s, pos);
+	for (int i_s = 0, pos = Search(s, s1, i_s), i = 0; pos >= 0;) {
 		Copy(result, s, i, i_s, pos - 1);
 		i += pos - i_s;
 		i_s = pos + strlen(s1);
@@ -64,16 +61,12 @@ char *Replace(char s[], const char s1[], const char s2[]) {
 			Copy(result, s, i, i_s, strlen(s) - 1);
 		}
 	}
-
-	printf("%s\n", result);
-
 	return result;
 }
 
 int main(int argc, char** argv) {
 	size_t length = 99;
 	char *s, *s1, *s2;
-
 	
 	s = (char*)malloc(sizeof(char) * length);
 	s1 = (char*)malloc(sizeof(char) * length);
@@ -87,11 +80,6 @@ int main(int argc, char** argv) {
 	RemoveNewLine(s1);
 	RemoveNewLine(s2);
 	
-
-	/*s = "111 11 111 1 2 111 11 1 111 1";
-	s1 = "111";
-	s2 = "x";*/
-
 	s = Replace(s, s1, s2);
 
 	printf("\nS \n%s\n", s);
